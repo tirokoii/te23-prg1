cc4af6420fb37650451ec0e122fd580433bbab53
tirokoii <tilli.edstrom@elev.ga.ntig.se>
2024-08-28 09:29:29 +0200

    Variables

---
+++ hello.py
@@ -0,0 +1,11 @@
+
+namn = input("Write your name: ")
+
+important = "The sign we use for strings is called fnuttar."
+
+print(f"{important} is very important {namn} should remember it.")
+
+print("It is very important that Tilli remembers it")
+
+
+print(type(namn))
