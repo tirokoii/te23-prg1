cc800dafcb140a2d5a3ab17f752562b3d876c840
tirokoii <tilli.edstrom@elev.ga.ntig.se>
2024-08-29 08:44:45 +0200

    Ändra namn

--- lektion1.py
+++ lektion1.py
@@ -1,11 +1,11 @@
 
-namn = input("Write your name: ")
+namn = input("Write your name:") 
 
 important = "The sign we use for strings is called fnuttar."
 
 print(f"{important} is very important {namn} should remember it.")
 
-print("It is very important that Tilli remembers it")
+print("It is very important that you remember it")
 
 
 print(type(namn))
