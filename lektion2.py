b8b62272b160e81a5b23bdc8fb3fb73946a063d7
tirokoii <tilli.edstrom@elev.ga.ntig.se>
2024-08-29 21:06:02 +0200

    create if statements

--- lektion2.py
+++ lektion2.py
@@ -2,27 +2,40 @@
 
 print("Hello and welcome to my program")
 
-firstName = input("Please write your name: ")
+while True:
 
-print(f"Good day {firstName}. Thank you for gracing me with your presence.") 
+    firstName = input("Please write your name: ")
 
-
-print("I would like to know more about you.")
-
-lastName = input("Please write your last name: ")
-
-print(f"Thanks for the answer {firstName} {lastName}.")
-
-
-age = input("Please write you age, in numbers: ")
-
-ageConverted = int(age)
-if int(ageConverted) <67:
-    print(f"Wow {age}! Soon you will retire.")
-else:
-    print(f"{age}, so young!")
-
-print(f"Thank for the answers {firstName} {lastName} the {age} year old.")
+    if firstName.isalpha():
+        print(f"Good day {firstName}. Thank you for gracing me with your presence.") 
+        break
+    else:
+        print("Please answer with letters...")
 
 
+while True:
 
+    lastName = input("Please write your last name: ")
+
+    if lastName.isalpha():
+        print(f"Thank you for the answer {firstName} {lastName}.") 
+        break
+    else:
+        print("Please answer with letters...")
+        
+while True:
+    age = input("Please write your age in numbers: ")
+
+    if age.isdigit():
+        age = int(age)
+        if age < 67:
+            print(f"Wow {age}! Soon you will retire.")
+        elif age > 67 and age < 200:
+            print(f"Wow {age}, so young!")
+        elif age > 200:
+            print(f"{age} interesting, how was it? the dinosaurs I mean.")
+        break
+    else:
+        print("Please write your age using only numbers...")
+        
+print(f"Thank for participating in this program {firstName} {lastName} the {age} year old.")
