a8791122c2bde5cbe8f42dc4ad339fadec326c34
tirokoii <tilli.edstrom@elev.ga.ntig.se>
2025-01-23 09:49:02 +0100

    övning

---
+++ exam1/2.1.py
@@ -0,0 +1,5 @@
+mätIdag = int(input("Mätarställning i dag?: "))
+mätFör = int(input("Mätarställning för ett år sedan?: "))
+mil = print(f"Antal körda mil: {(mätIdag-mätFör): .0f}")
+liter = print(f"Antal liter bensin: {((mätFör/mätIdag)*(mätIdag-mätFör)): .1f}")
+förbrukning = print(f"Förbrukning per mil: {(mätFör/mätIdag): .2f}")
