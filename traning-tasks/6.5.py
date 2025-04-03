59cc7f69f2d714cfed3bde2b29d84994d0ee86d1
tirokoii <tilli.edstrom@elev.ga.ntig.se>
2025-04-03 08:26:21 +0200

    organize files

--- traning-tasks/6.5.py
+++ traning-tasks/6.5.py
@@ -1,6 +1,7 @@
 # Calculate (Standardavikelse) for a couple of values
 # The numbers should be in a list
 # The formula: o = squrt((1/n)*sum(n)*(x - medel)(x - medel))
+# x = numbers
 
 from math import sqrt
 
@@ -9,9 +10,11 @@ x = []
 while len(x) < 10:
     x.append(int(input('skriv in ett värde: ')))
 
+f = 0
 total = 0
-for i in x:
-    total += i
-mid = total/len(x)
-
-o = sqrt((1/n)*sum(len(n))*(x -mid)(x - mid))
+while True:
+    for i in x:
+        total += i
+        f += (1/i)*sum(i)
+    mid = total/len(x)  
+    o = sqrt(f*(x -mid)(x - mid))
